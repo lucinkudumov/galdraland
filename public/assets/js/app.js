03735aaa895092222d43826d078e0a3a056b7b3d
Lucin Kudumov <lucinkudumov@gmail.com>
2016-02-20 23:54:58 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1639,7 +1639,6 @@ app.controller("teamViewController", ["$scope", "$http", "$stateParams", "User",
 		modalInstance.result.then(function (result) {
 			if (result.type == "CREATE") {
 				var request = $http({ method : "POST", url : "addMemberTitle", api : true, data : { team_id : result.team._id, titles : result.titles, defuser : $rootScope.defUser }});
-				location.reload();
 			}
 		});
 	}
