049007c82e187589d0abe6f25882f4d2792a3ed6
Lucin Kudumov <lucinkudumov@gmail.com>
2016-03-07 14:47:37 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("galdra", ["ngRoute", "ui.router", "ngCookies", "ui.bootstrap", "ngFileUpload"]);
+var app = angular.module("galdra", ["ngRoute", "ui.router", "ngCookies", "ui.bootstrap", "angularFileUpload"]);
 var config = {
     //siteurl : 'http://galdraland.com:9010/'
     siteurl: 'http://galdraland-1-0.herokuapp.com/'
