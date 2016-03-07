63e6fe26c96652f2a96fd8439d1613a6d08c67a7
Lucin Kudumov <lucinkudumov@gmail.com>
2016-03-07 08:58:11 +0800

    test

--- app/controllers/user/adventures.js
+++ app/controllers/user/adventures.js
@@ -1,4 +1,7 @@
 var validator = require('validator'),
+        uuid = require('node-uuid'),
+        multiparty = require('multiparty'),
+        fs = require('fs'),
         async = require('async');
 
 module.exports = function (opts) {
@@ -8,6 +11,32 @@ module.exports = function (opts) {
             adventureModel = opts.models.Adventure;
 
     return {
+        "post#upload/image": function (req, res) {
+            var form = new multiparty.Form();
+            form.parse(req, function (err, fields, files) {
+                var file = files.file[0];
+                var contentType = file.headers['content-type'];
+                var tmpPath = file.path;
+                var extIndex = tmpPath.lastIndexOf('.');
+                var extension = (extIndex < 0) ? '' : tmpPath.substr(extIndex);
+                // uuid is for generating unique filenames. 
+                var fileName = uuid.v4() + extension;
+                var destPath = 'path/to/where/you/want/to/store/your/files/' + fileName;
+
+                // Server side file type checker.
+                if (contentType !== 'image/png' && contentType !== 'image/jpeg') {
+                    fs.unlink(tmpPath);
+                    return res.status(400).send('Unsupported file type.');
+                }
+
+                fs.rename(tmpPath, destPath, function (err) {
+                    if (err) {
+                        return res.status(400).send('Image is not saved:');
+                    }
+                    return res.json(destPath);
+                });
+            });
+        },
         "post#adventure/create": function (req, res) {
             var name = req.body.name,
                     description = req.body.description,
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -295,6 +295,37 @@ app.controller("createAdventureController", ["$scope", "$rootScope", "$http", "$
             });
         }
 
+        $scope.onFileSelect = function(image) {
+            if (angular.isArray(image)) {
+                image = image[0];
+            }
+
+            // This is how I handle file types in client side
+            if (image.type !== 'image/png' && image.type !== 'image/jpeg') {
+                alert('Only PNG and JPEG are accepted.');
+                return;
+            }
+
+            $scope.uploadInProgress = true;
+            $scope.uploadProgress = 0;
+
+            $scope.upload = $upload.upload({
+                url: 'upload/image',
+                method: 'POST',
+                file: image
+            }).progress(function(event) {
+                $scope.uploadProgress = Math.floor(event.loaded / event.total);
+                $scope.$apply();
+            }).success(function(data, status, headers, config) {
+                $scope.uploadInProgress = false;
+                // If you need uploaded file immediately 
+                $scope.uploadedImage = JSON.parse(data);      
+            }).error(function(err) {
+                $scope.uploadInProgress = false;
+                console.log('Error uploading file: ' + err.message || err);
+            });
+        };
+
         $scope.post_to_fb = function (id) {
             FB.login(function () {
                 FB.api('/me/feed', 'post', {message: $scope.user.fullname + " has created a new adventure on Galdraland.\n" + config.siteurl + "/adventures/view/" + id});
