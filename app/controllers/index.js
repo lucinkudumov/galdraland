cef5c50a1d60274b0afbcda300494c9c18510704
Lucinkudumov <lucinkudumov@gmail.com>
2016-04-11 19:18:41 +0900

    test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,7 +10,7 @@ module.exports = function (opts) {
 //	var fail = "http://galdraland-1-0.herokuapp.com/";
     
     return {
-        "get#login/facebook" : [function(req,res,next){req.session.returnTo = "/api/myTeams";/*req.query.r*/ console.log(req.query); next();}, passport.authenticate('facebook', { scope: ['user_photos', 'email'] })],
+        "get#login/facebook" : [function(req,res,next){req.session.returnTo = "/myTeams"; console.log(req.params.type);/*req.query.r*/ console.log(req.query); next();}, passport.authenticate('facebook', { scope: ['user_photos', 'email'] })],
         "get#callback/facebook" : passport.authenticate('facebook', { failureRedirect: fail, successRedirect : "/api/cookie" }),
     }
 }
