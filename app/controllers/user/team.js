49cfe79a0f739f54d1ba6cb9a29fe2ddb3ace9e5
Lucinkudumov <lucinkudumov@gmail.com>
2016-05-10 20:51:09 +0200

    all image

--- app/controllers/user/team.js
+++ app/controllers/user/team.js
@@ -10,9 +10,9 @@ module.exports = function (opts) {
         "post#upload/image": function (req, res) {
             var file = req.files.file;
             if (file !== 'undefined') {
-                if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
-                    return res.json({success: false, error: "Image file type error"});
-                }
+//                if (file.type !== 'image/png' && file.type !== 'image/jpeg') {
+//                    return res.json({success: false, error: "Image file type error"});
+//                }
 
                 fs.readFile(file.path, function (err, data) {
                     if (err) {
