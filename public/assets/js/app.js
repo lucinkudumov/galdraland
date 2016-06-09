66ed22d9d40aea3ebea15ac708e0b043f92bbe2e
David Makow <dav.makow1992@yandex.com>
2016-06-09 20:12:15 +0200

    comment change 6.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1871,7 +1871,7 @@ app.directive('commentWidget', function ($http, User) {
         scope: {
             ref: '=ref',
         },
-        template: '<div class="row"><div class="user-container"><h4>Comments</h4><hr></div><div id="dv1"><ul><li ng-repeat="comnt in comments"> <img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} </li></ul></div></div>' + '<div class="row"><div class="user-container"><h4>Comments</h4><hr></div></div>',
+        template: '<div class="row"><div class="user-container"><h4>Comments</h4><hr></div><div id="dv1"><ul><li ng-repeat="comnt in comments"> <img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} </li></ul></div><div class="user-container"><h4>Post your comment</h4><hr></div><div><textarea ng-model="txtcomment" placeholder="Your Comment" style="width:550px"></textarea><button ng-click="save();" style="margin-top:10px;">Post Comment</button></div></div>',
         link: function (scope, elem, attrs) {
             scope.comment = [];
             scope.user = User.isLoggedIn();
