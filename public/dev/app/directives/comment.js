f1a53050a8abce4217fdaa5683906d49127f118d
David Makow <dav.makow1992@yandex.com>
2016-06-09 19:55:57 +0200

    comment change 4.

--- public/dev/app/directives/comment.js
+++ public/dev/app/directives/comment.js
@@ -5,8 +5,7 @@ app.directive('commentWidget', function ($http, User) {
 		scope: {
 			ref: '=ref',
 		},
-		template: '<div class="row"><div class="user-container"><h4>Comments</h4><hr></div><div id="dv1"><ul><li ng-repeat="comnt in comments"> <img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} </li></ul></div></div>' +
-		          '<div class="row"><div class="user-container"><h4>Post your Comment</h4><hr></div><textarea ng-model="txtcomment" placeholder="Your Comment" style="width:550px"></textarea><button ng-click="save();" style="margin-top:10px;">Post Comment</button></div>',
+		template: '<div class="row"><div class="user-container"><h4>Comments</h4><hr></div><div id="dv1"><ul><li ng-repeat="comnt in comments"> <img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} </li></ul></div></div>' + 		          '<div class="row"><div class="user-container"><h4>Post your Comment</h4><hr></div><textarea ng-model="txtcomment" placeholder="Your Comment" style="width:550px"></textarea><button ng-click="save();" style="margin-top:10px;">Post Comment</button></div>',
 		link: function (scope, elem, attrs) {
 			scope.comment = [];
 			scope.user = User.isLoggedIn();
