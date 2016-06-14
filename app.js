9eb2c1aeef238af537e83fc464d181397964e4fa
David Makow <dav.makow1992@yandex.com>
2016-06-14 20:01:13 +0200

    Image keep test 19

--- app.js
+++ app.js
@@ -36,17 +36,6 @@ app.configure("production", function () {
 });
 
 app.configure(function () {
-    //Image rendering...
-    app.get('/assets/images/upload/:id',function(req,res,next){
-        console.log(req);
-        imageModel.findOne({name: req.id},function (err, image) {
-            if (err) return next(err);
-        // var base64 = (doc[0].img.data.toString('base64'));
-        //  res.send(base64);
-            res.writeHead('200', {'Content-Type': 'image/png'});
-            res.end(image.data.data, 'binary');
-        });
-    });
     //Public--------------------------------------
     app.use(express.static(__dirname + '/public'));
     app.use(express.cookieParser());
--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,5 +1,16 @@
+var imageModel = require('../models/Imagestore');
 module.exports = function (opts) {
-    return function (req, res, next) {
-        
-    }
+    return {
+	    //Image rendering...
+	    app.get('/assets/images/upload/:id',function(req,res,next){
+	        console.log(req);
+	        imageModel.findOne({name: req.id},function (err, image) {
+	            if (err) return next(err);
+	        // var base64 = (doc[0].img.data.toString('base64'));
+	        //  res.send(base64);
+	            res.writeHead('200', {'Content-Type': 'image/png'});
+	            res.end(image.data.data, 'binary');
+	        });
+	    });
+  }
 }
