e46c8040c7017b0d1141a3bb8e321d98b16e3fb7
David Makow <dav.makow1992@yandex.com>
2016-06-14 15:51:31 +0200

    Image keep test 10

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,17 +1,5 @@
 module.exports = function (opts) {
-		// var imageModel = opts.models.image;
-		var imageModel1 = opts.models.Image;
     return function (req, res, next) {
-        //Image loading from DB.
-           app.get('assets/images/upload/:id', function(req, res) {
-                console.log(req);
-                imageModel.findOne({name: req.id},function (err, image) {
-                    if (err) return next(err);
-                // var base64 = (doc[0].img.data.toString('base64'));
-                //  res.send(base64);
-                    res.writeHead('200', {'Content-Type': 'image/png'});
-                    res.end(image.data.data, 'binary');
-                });
-            });
+        
     }
 }
