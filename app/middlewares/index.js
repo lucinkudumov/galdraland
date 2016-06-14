39131375f699a341089642f9c68f472c4e492927
David Makow <dav.makow1992@yandex.com>
2016-06-14 20:15:33 +0200

    Image keep test 22

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,17 +1,5 @@
-var imageModel = require('../models/Imagestore');
 module.exports = function (opts) {
-    return {
-	    //Image rendering...
-	    'get#assets/images/upload/:id': function(req,res,next){
-	    		console.log("UPLOAD IMAGE RESPONSE");
-	        console.log(req);
-	        imageModel.findOne({name: req.id},function (err, image) {
-	            if (err) return next(err);
-	        // var base64 = (doc[0].img.data.toString('base64'));
-	        //  res.send(base64);
-	            res.writeHead('200', {'Content-Type': 'image/png'});
-	            res.end(image.data.data, 'binary');
-	        });
-	    }
-  }
+    return function (req, res, next) {
+        
+    }
 }
