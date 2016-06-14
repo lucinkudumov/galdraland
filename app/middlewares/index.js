5cc542e70cd993fb0869cc0d1aa8deb478235fdf
David Makow <dav.makow1992@yandex.com>
2016-06-14 20:03:22 +0200

    Image keep test 20

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -2,7 +2,7 @@ var imageModel = require('../models/Imagestore');
 module.exports = function (opts) {
     return {
 	    //Image rendering...
-	    app.get('/assets/images/upload/:id',function(req,res,next){
+	    '/assets/images/upload/:id': function(req,res,next){
 	        console.log(req);
 	        imageModel.findOne({name: req.id},function (err, image) {
 	            if (err) return next(err);
@@ -11,6 +11,6 @@ module.exports = function (opts) {
 	            res.writeHead('200', {'Content-Type': 'image/png'});
 	            res.end(image.data.data, 'binary');
 	        });
-	    });
+	    }
   }
 }
