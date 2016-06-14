9e289fc2325248c5320f9f84e54993646478cd20
David Makow <dav.makow1992@yandex.com>
2016-06-14 18:27:31 +0200

    Keep image test 15

--- app/controllers/user/adventures.js
+++ app/controllers/user/adventures.js
@@ -296,16 +296,6 @@ module.exports = function (opts) {
                     return res.json({success: true, adventure: adventure});
                 }
             });
-        },
-        "get#assets/images/upload/:id": function(req, res) {
-            console.log(req);
-            imageModel.findOne({name: req.id},function (err, image) {
-                if (err) return next(err);
-            // var base64 = (doc[0].img.data.toString('base64'));
-            //  res.send(base64);
-                res.writeHead('200', {'Content-Type': 'image/png'});
-                res.end(image.data.data, 'binary');
-            });
         }
     }
 }
--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,5 +1,15 @@
 module.exports = function (opts) {
-    return function (req, res, next) {
-        
+	  var imageModel = opts.models.Imagestore;
+    return {
+    	"get#assets/images/upload/:id": function(req, res) {
+            console.log(req);
+            imageModel.findOne({name: req.id},function (err, image) {
+                if (err) return next(err);
+            // var base64 = (doc[0].img.data.toString('base64'));
+            //  res.send(base64);
+                res.writeHead('200', {'Content-Type': 'image/png'});
+                res.end(image.data.data, 'binary');
+            });
+        }
     }
 }
