c5f55c4fd0685395280d7ae8d5e9d813fac6ba19
David Makow <dav.makow1992@yandex.com>
2016-06-14 19:17:05 +0200

    Image keep test 17

--- app.js
+++ app.js
@@ -36,16 +36,16 @@ app.configure("production", function () {
 });
 
 //Image rendering...
-app.get('/assets/images/upload/:id',function(req,res,next){
-    console.log(req);
-    imageModel.findOne({name: req.id},function (err, image) {
-        if (err) return next(err);
-    // var base64 = (doc[0].img.data.toString('base64'));
-    //  res.send(base64);
-        res.writeHead('200', {'Content-Type': 'image/png'});
-        res.end(image.data.data, 'binary');
-    });
-});
+// app.get('/assets/images/upload/:id',function(req,res,next){
+//     console.log(req);
+//     imageModel.findOne({name: req.id},function (err, image) {
+//         if (err) return next(err);
+//     // var base64 = (doc[0].img.data.toString('base64'));
+//     //  res.send(base64);
+//         res.writeHead('200', {'Content-Type': 'image/png'});
+//         res.end(image.data.data, 'binary');
+//     });
+// });
 
 app.configure(function () {
     //Public--------------------------------------
