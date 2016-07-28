ba44b6c393018b36cd06ad174e4a8b3ea699dbd3
David Makow <dav.makow1992@yandex.com>
2016-07-28 20:24:04 +0200

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2204,11 +2204,11 @@ app.directive('commentWidget', function ($http, User) {
             '<div id="dv1">' +
             '<ul>' +
             '<li ng-repeat="comnt in comments"> ' +
-            '<img src="{{ comnt.from.photo }}" style="width:50px;height:50px;"><span  style="width:350px;">{{comnt.from.fullname}}: {{ comnt.comment }}</span>' +
+            '<img src="{{ comnt.from.photo }}" style="width:50px;height:50px;"><span>{{comnt.from.fullname}}: {{ comnt.comment }}' +
             '<span ng-show="ismanager">' +
             '<a class="btn btn-danger" style="float: right;">Reject</a>' +
             '<a class="btn btn-primary" style="float: right;">Approve</a>' +
-            '</span>' +
+            '</span></span>' +
             '</li>' +
             '</ul>' +
             '</div>' +
