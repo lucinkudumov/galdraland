5ad49c1e0fd36cfb29791957f401431dfb6be96e
David Makow <dav.makow1992@yandex.com>
2016-07-28 18:22:30 +0200

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2203,7 +2203,12 @@ app.directive('commentWidget', function ($http, User) {
             '<div id="dv1">' +
             '<ul>' +
             '<li ng-repeat="comnt in comments"> ' +
-            '<img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} </li>' +
+            '<img src="{{ comnt.from.photo }}" style="width:50px;height:50px;">{{comnt.from.fullname}}: {{ comnt.comment }} ' +
+            '<span ng-show="isManager">' +
+            '<a class="btn btn-primary">Approve</a>' +
+            '<a class="btn btn-danger">Reject</a>' +
+            '</span>' +
+            '</li>' +
             '</ul>' +
             '</div>' +
             '<div class="user-container"><h4>Post your comment</h4><hr></div><div class="row" style="width:100%"><textarea ng-model="txtcomment" placeholder="Your Comment" style="width:100%;display:block;"></textarea><button ng-click="save();" style="margin-top:10px;float:right;">Post Comment</button></div></div>',
