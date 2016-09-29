55f377851379f4f71e491a4a2fb504c7abe5aae0
DavidMakow <dav.makow1992@yandex.com>
2016-09-30 05:58:03 +0800

    Revert "add multi tags"
    This reverts commit 149c1634d4206c8654fdd60f516b654c7c960c35.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -148,7 +148,7 @@ app.config(["$urlRouterProvider", "$locationProvider", "$stateProvider", "$httpP
             },
             requireLogin: true
         }).state("adventureTagList", {
-            url: "/adventuresTag/:tag",
+            url: "/adventuresTag/:id",
             views: {
                 "main": {templateUrl: "/assets/partials/main.html"},
                 "left-side@adventureTagList": {templateUrl: "/assets/partials/adventureTag/left-side.html"},
