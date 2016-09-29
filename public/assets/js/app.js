5e0dc4845ca04c225bfcabb8e2dff1320cd0078a
DavidMakow <dav.makow1992@yandex.com>
2016-09-29 17:01:30 +0800

    add users page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -305,12 +305,12 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
 app.controller("usersResultController", ["$scope", "$http", "User", "$location", function ($scope, $http, User, $location) {
     $scope.user = User.isLoggedIn();
 
-    $scope.getUserById = function (userId) {
-        var request = $http({method: "POST", url: "getUserById", api: true, data: {id: userId}});
-        request.success(function (data) {
-            console.log("user = ", data.user);
-        });
-    }
+//    $scope.getUserById = function (userId) {
+//        var request = $http({method: "POST", url: "getUserById", api: true, data: {id: userId}});
+//        request.success(function (data) {
+//            console.log("user = ", data.user);
+//        });
+//    }
 
     $scope.refresh = function () {
         $scope.loading = true;
@@ -325,7 +325,7 @@ app.controller("usersResultController", ["$scope", "$http", "User", "$location",
                             var o = $scope.teams[i].teamMembers[j];
 
                             console.log("userId = " + o.user);
-                            $scope.getUserById(o.user);
+//                            $scope.getUserById(o.user);
                         }
                     }
                     $scope.loading = false;
