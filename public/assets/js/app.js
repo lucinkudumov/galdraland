b8d27fc61d140f69749decfe421728255fadf673
DavidMakow <dav.makow1992@yandex.com>
2016-11-03 03:09:17 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2430,7 +2430,7 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
                 $compile($('#fbPage'))($scope);
             }
 
-            var htmlcontent = "<div id='fb-root'></div><script>window.fbAsyncInit = function () {FB.init({appId: '110469289012320',status: true,cookie: true,xfbml: true,version: 'v2.6'});};window.fbAsyncInit();(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s);js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class='fb-comments' data-href='http://webascender.com/blog' data-numposts='5' data-colorscheme='light'></div>";
+            var htmlcontent = "<div id='fb-root'></div><script>window.fbAsyncInit = function () {FB.init({appId: '110469289012320',status: true,cookie: true,xfbml: true,version: 'v2.6'});};window.fbAsyncInit();(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s);js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class='fb-comments' data-href='http://galdraland-1-0.herokuapp.com/teams/view/" + $stateParams.id + "' data-numposts='5' data-colorscheme='light'></div>";
             var $scope = $('#fbComment').html(htmlcontent).scope();
             $compile($('#fbComment'))($scope);
 
