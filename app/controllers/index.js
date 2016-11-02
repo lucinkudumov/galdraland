3b6f001f23cc5e0cb476efca5044bf2e8e585254
DavidMakow <dav.makow1992@yandex.com>
2016-11-03 04:07:22 +0800

    test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,8 +11,8 @@ module.exports = function (opts) {
 //	var fail = "http://galdraland-1-0.herokuapp.com/";
     
     return {
-//        "get#login/facebook" : [function(req,res,next){if (req.query.type == "teams") req.session.returnTo = "/teams/view/" + req.query.id; else if(req.query.type == "adventures") req.session.returnTo = "/adventures/view/" + req.query.id; else req.session.returnTo =req.query.r; console.log(req.query.type); console.log(req.query.id); console.log(req.query); next();}, passport.authenticate('facebook', { scope: ['user_photos', 'email'] })],
-//        "get#callback/facebook" : passport.authenticate('facebook', { failureRedirect: fail, successRedirect : "/api/cookie" }),
+        "get#login/facebook" : [function(req,res,next){if (req.query.type == "teams") req.session.returnTo = "/teams/view/" + req.query.id; else if(req.query.type == "adventures") req.session.returnTo = "/adventures/view/" + req.query.id; else req.session.returnTo =req.query.r; console.log(req.query.type); console.log(req.query.id); console.log(req.query); next();}, passport.authenticate('facebook', { scope: ['user_photos', 'email'] })],
+        "get#callback/facebook" : passport.authenticate('facebook', { failureRedirect: fail, successRedirect : "/api/cookie" }),
         'get#assets/images/upload/:id':function(req,res,next){
                 console.log("Uploaded Image Request...");
                 var id = req.param('id');
