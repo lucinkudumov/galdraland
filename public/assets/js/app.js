5ebb7f7594d81969ae63ba7b290dbd6b99050e7a
DavidMakow <dav.makow1992@yandex.com>
2016-11-18 17:05:12 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2480,7 +2480,7 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
 
 app.directive('dynFbCommentBox', function () {
     function createHTML(href, numposts, colorscheme) {
-        return '<div class="fb-comments" ' +
+        return '<div class="fb-comment-embed" ' +
             'data-href="' + href + '" ' +
             'data-numposts="' + numposts + '" ' +
             'data-colorsheme="' + colorscheme + '">' +
