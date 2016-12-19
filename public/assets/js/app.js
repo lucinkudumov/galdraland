dbedc71b0ac76c033c8d44a2186c4fcef8f1ab19
DavidMakow <dav.makow1992@yandex.com>
2016-12-19 20:43:27 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -306,48 +306,41 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
         simpleCalendarConfig.onDayClick = onDayClick;
         simpleCalendarConfig.onEventClick = onEventClick;
 
-        $scope.date = new Date();
-        $scope.events = [{
-            name: 'foo',
-            date: '5-2-15'
-        }, {
+        $scope.date = moment().toDate();
+        $scope.events = [ {
             name: 'bar',
             date: new Date()
-        }, {
-            name: 'bar foo',
+        },{
+            name: 'bar 2',
             date: new Date()
-        }, {
+        },{
             name: 'baz',
             date: '6-12-15'
         }];
 
-        console.log("events = ", $scope.events);
-
         $scope.changeMonth = changeMonth;
         $scope.monthName = monthName;
 
+        function monthName(date){
+            var months = [
+                'January','February','March',
+                'April','May','June',
+                'July','August','September',
+                'October','November','December',
+            ];
+            return months[date.getMonth()];
+        }
+
         function onDayClick(day){
-            console.log(day);
+            alert(JSON.stringify(day, null, 2))
         }
 
         function onEventClick(event, day){
-            console.log(event, day);
+            alert(JSON.stringify(event, null, 2) +'\n' + JSON.stringify(day, null, 2))
         }
 
-        function monthName(date) {
-            var d = new Date(date);
-            var months = [
-                'January', 'February', 'March',
-                'April', 'May', 'June',
-                'July', 'August', 'September',
-                'October', 'November', 'December'
-            ];
-            return months[d.getMonth()];
-        }
-
-        function changeMonth(offset) {
-            var d = new Date($scope.date);
-            $scope.date = d.setMonth(d.getMonth() + offset);
+        function changeMonth(offset){
+            $scope.date = moment($scope.date).add(offset, 'month').toDate();
         }
 
         $scope.refresh = function () {
