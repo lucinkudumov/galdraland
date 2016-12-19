6ef826ed302f83a20a9ae6890a6dffd52d01e8ea
DavidMakow <dav.makow1992@yandex.com>
2016-12-19 18:38:09 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -306,51 +306,46 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
         simpleCalendarConfig.onDayClick = onDayClick;
         simpleCalendarConfig.onEventClick = onEventClick;
 
-//        $scope.date = new Date();
-        $scope.date = moment().toDate();
-        console.log("Date = " + $scope.date);
-        $scope.events = [
-            {
-                name: 'foo',
-                date: '5-2-15',
-                link: 'http://www.google.com'
-            }, {
-                name: 'bar',
-                date: new Date()
-            },{
-                name: 'bar 2',
-                date: new Date()
-            },{
-                name: 'baz',
-                date: '6-12-15'
-            }
-        ];
+        $scope.date = new Date();
+        $scope.events = [{
+            name: 'foo',
+            date: '5-2-15'
+        }, {
+            name: 'bar',
+            date: new Date()
+        }, {
+            name: 'bar foo',
+            date: new Date()
+        }, {
+            name: 'baz',
+            date: '6-12-15'
+        }];
 
         $scope.changeMonth = changeMonth;
         $scope.monthName = monthName;
 
-        function monthName(date){
-            var months = [
-                'January','February','March',
-                'April','May','June',
-                'July','August','September',
-                'October','November','December',
-            ];
-            return months[date.getMonth()];
-        }
-
         function onDayClick(day){
-            console.log("onDayClick");
-            alert(JSON.stringify(day, null, 2))
+            console.log(day);
         }
 
         function onEventClick(event, day){
-            console.log("onEventClick");
-            alert(JSON.stringify(event, null, 2) +'\n' + JSON.stringify(day, null, 2))
+            console.log(event, day);
         }
 
-        function changeMonth(offset){
-            $scope.date = moment($scope.date).add(offset, 'month').toDate();
+        function monthName(date) {
+            var d = new Date(date);
+            var months = [
+                'January', 'February', 'March',
+                'April', 'May', 'June',
+                'July', 'August', 'September',
+                'October', 'November', 'December'
+            ];
+            return months[d.getMonth()];
+        }
+
+        function changeMonth(offset) {
+            var d = new Date($scope.date);
+            $scope.date = d.setMonth(d.getMonth() + offset);
         }
 
         $scope.refresh = function () {
