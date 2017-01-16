2ccca67ccc06e9ae81e9eb32f3328fa558012594
DavidMakow <dav.makow1992@yandex.com>
2017-01-16 12:11:28 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -453,7 +453,10 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
 
         $scope.$watch("adventure._id", function(newValue, oldValue){
             if (newValue != oldValue) {
-                var htmlcontent = "<html><head><title>This is test site</title></head><body>" +
+                var htmlcontent = "<html><head>" +
+                    "<title>This is test site</title>" +
+                    "<meta property='og:description'   content='Your description' />" +
+                    "</head><body>" +
                     "<div id='fb-root'>" +
                     "</div><script>window.fbAsyncInit = function () {FB.init({appId: '110469289012320',status: true,cookie: true,xfbml: true,version: 'v2.6'});};window.fbAsyncInit();(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s);js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>" +
                     "<div class='fb-share-button' data-href='http://galdraland-1-0.herokuapp.com/adventures/view/"+newValue+"' data-layout='button_count'></div>" +
