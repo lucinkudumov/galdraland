842cf264c26972c8c8e4186bc3e3565daa57cf9c
David Makow <dav.makow1992@yandex.com>
2017-01-25 16:51:31 +0800

    test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app       = express(),
     secretKey = "hJKQg7dxMGzEWqf",
     smtpTransport = nodemailer.createTransport("SMTP", config.smtpOptions),
     templatesDir = path.join(__dirname, "emails");
-//const butter = require('buttercms')('1582edd571779ca918db1f34a6b43e6a962d9471');
+var butter = require('buttercms')('1582edd571779ca918db1f34a6b43e6a962d9471');
 
 if (process.env.REDISTOGO_URL) {
     var rtg = require("url").parse(process.env.REDISTOGO_URL);
