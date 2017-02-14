defce8424105e4c2eaf5db4f1008ff3d7348341b
David Makow <dav.makow1992@yandex.com>
2017-02-15 02:13:35 +0800

    add debug

--- app/controllers/user/teamblog.js
+++ app/controllers/user/teamblog.js
@@ -12,19 +12,23 @@ module.exports = function (opts) {
                 body = req.body.blogBody,
                 team = req.body.team,
                 teamblog = new teamBlogModel();
+            console.log("createblog..... teamid = ", team);
             teamModel.findOne({id: team}, function (err, team) {
                 if (err) {
                     console.log(err);
                     return res.json({success: false, error: "Internal server error"});
                 } else if (team) {
+                    console.log("find OK!");
                     teamblog.title = title;
                     teamblog.image = image;
                     teamblog.body = body;
                     teamblog.team = team;
                     teamblog.save(function (err, teamblog) {
+                        console.log("saving blog....");
                         if (err) {
                             console.log(err);
                         } else {
+                            console.log("saving blog OK");
                             return res.json({success: true, id: teamblog._id});
                         }
                     });
