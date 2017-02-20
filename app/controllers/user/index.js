253b0ff376b04368f499556d18ee0a2f6a731a81
David Makow <dav.makow1992@yandex.com>
2017-02-20 22:13:44 +0800

    test

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -35,6 +35,7 @@ module.exports = function (opts) {
             });
         },
         "post#getUserById": function (req, res) {
+            console.log("getUserById = " + req.body.id);
             userModel.findOne({_id: req.body.id}).select("_id profileId fullname username email signin photo skype experience goals categories").exec(function (err, user) {
                 if (err) {
                     console.log(err);
