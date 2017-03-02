13beb594e7cb664ed72ec807a158fc43b6e05c8f
David Makow <dav.makow1992@yandex.com>
2017-03-03 02:49:44 +0800

    test

--- app/controllers/user/slack.js
+++ app/controllers/user/slack.js
@@ -64,6 +64,27 @@ module.exports = function (opts) {
             });
         },
         "post#slack/sendInvite": function (req, res) {
+            userModel.findOne({_id: req.user._id}, function (err, user) {
+                if (err) {
+                    console.log(err);
+                } else if (user) {
+                    console.log(user.email);
+                    request.get({
+                        url: 'https://galdralandmarketing.slack.com/api/users.admin.invite?token='+user.slackToken+'&email='+user.email+'&set_active=true'
+                    }, function (err, response) {
+                        if(err) {
+                            console.log("error");
+                            return res.json({success: false});
+                        } else {
+                            console.log("success");
+                            var result = JSON.parse(response.body);
+                            console.log("sendInvite = ", result);
+                            return res.json({success: true});
+                        }
+                    });
+                }
+            });
+            /*
             var inviteId = req.body.id;
             inviteModel.findOne({_id: inviteId}, function (err, invite) {
                 if (err) {
@@ -72,9 +93,21 @@ module.exports = function (opts) {
                 } else if (invite) {
                     console.log("from = " + invite.from);
                     console.log("to = " + invite.toId);
-                    return res.json({success: true});
+                    userModel.findOne({_id: req.user._id}, function (err, user) {
+                        if (err) {
+                            return res.json({success: false});
+                        } else if (user) {
+                            user.slackToken = result.access_token;
+                            console.log(user.)
+                            user.save(function (err) {
+                                if (err) return res.json({success: false});
+                            });
+                        }
+                    });
+
                 }
             });
+            */
         }
     }
 }
