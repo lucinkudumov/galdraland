45736c7e9541a3fa42e53b718416b08f8af2e583
David Makow <dav.makow1992@yandex.com>
2017-03-02 17:14:20 +0800

    test

--- app/controllers/user/slack.js
+++ app/controllers/user/slack.js
@@ -17,25 +17,24 @@ module.exports = function (opts) {
         },
         "get#slack/auth" : function (req, res, next) {
             console.log("1111111");
-            console.log("code = " + req.param('code'));
-            request.get({
-                url: 'https://slack.com/api/oauth.access?client_id=146827931650.146151726865' +
-                    '&client_secret=80c8c252dabe4cbc46cfe0e29fb6272c' +
-                    '&redirect_uri=https://galdraland-1-0.herokuapp.com/api/slack/authtoken' +
-                    '&code=' + req.param('code')
-            }, function (err, response) {
-                if(err)
-                    console.log("err = ", err);
-                else
-                    console.log("response = ", response);
-            });
-
-//            console.log("req = ", req);
-//            console.log("res= ", res);
-//            console.log("next= ", next);
-        },
-        "get#slack/authtoken" : function (req, res, next) {
-            console.log("22222");
+            if (req.param('code') != null) {
+                console.log("aaaaa");
+                console.log("code = " + req.param('code'));
+                request.get({
+                    url: 'https://slack.com/api/oauth.access?client_id=146827931650.146151726865' +
+                        '&client_secret=80c8c252dabe4cbc46cfe0e29fb6272c' +
+                        '&redirect_uri=https://galdraland-1-0.herokuapp.com/api/slack/auth' +
+                        '&code=' + req.param('code')
+                }, function (err, response) {
+                    if(err)
+                        console.log("err = ", err);
+                    else
+                        console.log("response = ", response);
+                });
+            } else {
+                console.log("bbbbb");
+//                console.log("code = " + req.param('code'));
+            }
         }
     }
 }
