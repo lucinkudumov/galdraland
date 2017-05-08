22717dc1f91e80b6a9167e580e3df0ba83df6cbb
David Makow <dav.makow1992@yandex.com>
2017-05-08 14:54:49 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module("galdra", ["ngRoute", "ui.router", "ngCookies", "ui.bootstrap", "ngFileUpload", 'decipher.tags', 'ui.bootstrap.typeahead', /*'ngTagsInput',*/ "envoc.simpleCalendar", "infinite-scroll"/*, "meow.blog.view", "meow.blog.edit"*/]);
 var config = {
     //siteurl : 'http://galdraland.com:9010/'
-    siteurl: 'http://galdraland-1-0.herokuapp.com/'
+    siteurl: 'https://galdraland-1-0.herokuapp.com/'
 }
 
 app.config(["$urlRouterProvider", "$locationProvider", "$stateProvider", "$httpProvider", "$qProvider", function ($urlRouterProvider, $locationProvider, $stateProvider, $httpProvider, $qProvider) {
@@ -710,7 +710,7 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
                     "</div>" +
                     "<script>window.fbAsyncInit = function () {FB.init({appId: '110469289012320',status: true,cookie: true,xfbml: true,version: 'v2.6'});};window.fbAsyncInit();(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s);js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));" +
                     "function fbshare() {" +
-                    "window.open('https://www.facebook.com/sharer/sharer.php?app_id=110469289012320&sdk=joey&u=http://galdraland-1-0.herokuapp.com/adventures/view/"+newValue+"&display=popup&ref=plugin&src=share_button&description="+$scope.description+"&picture=http://galdraland-1-0.herokuapp.com"+$scope.adventure.image+"', '','width=200,height=100');" +
+                    "window.open('https://www.facebook.com/sharer/sharer.php?app_id=110469289012320&sdk=joey&u=https://galdraland-1-0.herokuapp.com/adventures/view/"+newValue+"&display=popup&ref=plugin&src=share_button&description="+$scope.description+"&picture=https://galdraland-1-0.herokuapp.com"+$scope.adventure.image+"', '','width=200,height=100');" +
                     "}" +
                     "</script>" +
 //                    "<div class='fb-share-button' onclick='a();' data-layout='button_count'></div>" +
@@ -731,7 +731,7 @@ app.controller("adventureViewController", ["$scope", "$http", "$stateParams", "$
             console.log("advId = " + $stateParams.id);
             FB.ui({
                 method: 'share',
-                href : 'http://galdraland-1-0.herokuapp.com/adventures/view/' + $stateParams.id
+                href : 'https://galdraland-1-0.herokuapp.com/adventures/view/' + $stateParams.id
             }, function(response){
                 console.log("response = ", response);
             });
@@ -5033,7 +5033,7 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
                     "</div><script>window.fbAsyncInit = function () {FB.init({appId: '110469289012320',status: true,cookie: true,xfbml: true,version: 'v2.6'});};window.fbAsyncInit();(function (d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s);js.id = id;js.src = '//connect.facebook.net/en_US/sdk.js';fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));" +
 //                    "<div class='fb-share-button' data-href='http://galdraland-1-0.herokuapp.com/teams/view/"+newValue+"' data-layout='button_count'></div>";
                     "function fbshare() {" +
-                    "window.open('https://www.facebook.com/sharer/sharer.php?app_id=110469289012320&sdk=joey&u=http://galdraland-1-0.herokuapp.com/teams/view/"+newValue+"&display=popup&ref=plugin&src=share_button&description="+$scope.description+"&picture=http://galdraland-1-0.herokuapp.com"+$scope.team.image+"', '','width=200,height=100');" +
+                    "window.open('https://www.facebook.com/sharer/sharer.php?app_id=110469289012320&sdk=joey&u=https://galdraland-1-0.herokuapp.com/teams/view/"+newValue+"&display=popup&ref=plugin&src=share_button&description="+$scope.description+"&picture=https://galdraland-1-0.herokuapp.com"+$scope.team.image+"', '','width=200,height=100');" +
                     "}" +
                     "</script>" +
                     "<img onclick='fbshare();' style='cursor:pointer;' src='/assets/images/fbshare.png'/>";
