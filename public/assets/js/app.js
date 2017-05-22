6facced6361a5fd90fa16c06850e1efdecd410cc
David Makow <dav.makow1992@yandex.com>
2017-05-23 01:32:42 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4939,8 +4939,19 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
                     $scope.adventures = data.data.advs;
                     $scope.isManager = data.data.team.owner._id == $scope.user._id;
                     $scope.ownerId = data.data.team.owner._id;
-                    $scope.position.lat = parseFloat(data.data.team.latitude);
-                    $scope.position.lng = parseFloat(data.data.team.longitude);
+
+                    if (data.data.team.latitude && isNaN(data.data.team.latitude)) {
+                        $scope.position.lat = parseFloat(data.data.team.latitude);
+                    } else {
+                        $scope.position.lat = 0 ;
+                    }
+
+                    if (data.data.team.longitude && isNaN(data.data.team.longitude)) {
+                        $scope.position.lng = parseFloat(data.data.team.longitude);
+                    } else {
+                        $scope.position.lng = 0 ;
+                    }
+
 //                    angular.extend($scope, {
 //                        position: {
 //                            lat: data.data.team.latitude,
