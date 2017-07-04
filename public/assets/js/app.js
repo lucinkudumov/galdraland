c3a111640e78033c47d9a1eb1e01956826740dd6
David Makow <dav.makow1992@yandex.com>
2017-07-04 14:34:52 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5089,21 +5089,11 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
     $scope.emptyRecMembers = [];
     $scope.slackAuthentication = false;
 
-    var mainMarker = {
-        lat: 51,
-        lng: 0,
-        focus: true,
-        draggable: false
-    };
-
     angular.extend($scope, {
         position: {
             lat: 51.505,
             lng: -0.09,
             zoom: 4
-        },
-        markers: {
-            mainMarker: angular.copy(mainMarker)
         }
     });
 
@@ -5145,18 +5135,14 @@ app.controller("teamViewController", ["$rootScope", "$scope", "$http", "$sce", "
 
                     if (data.data.team.latitude && !isNaN(data.data.team.latitude)) {
                         $scope.position.lat = parseFloat(data.data.team.latitude);
-                        $scope.markers.mainMarker.lat = $scope.position.lat;
                     } else {
                         $scope.position.lat = 0;
-                        $scope.markers.mainMarker.lat =0;
                     }
 
                     if (data.data.team.longitude && !isNaN(data.data.team.longitude)) {
                         $scope.position.lng = parseFloat(data.data.team.longitude);
-                        $scope.markers.mainMarker.lng = $scope.position.lng;
                     } else {
                         $scope.position.lng = 0;
-                        $scope.markers.mainMarker.lng = 0;
                     }
 
                     $scope.isMember = false;
