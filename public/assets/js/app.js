49ebbf83f999c3c0b9e7825c14febb1a33ae7882
David Makow <dav.makow1992@yandex.com>
2017-07-18 00:54:03 +0800

    test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4118,8 +4118,8 @@ app.controller("editTeamController", ["$scope", "$http", "$location", "$statePar
                         url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
                         type: 'xyz',
                         layerOptions: {
-                            apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-                            mapid: 'bufanuvols.lia22g09'
+                            apikey: 'pk.eyJ1IjoiZGF2aWRtYWtvdyIsImEiOiJjajU4ZTJiNnYxY203MzJuc2V5MnpvamVlIn0.8WpLniKXRbxJ7CPu_72yVA',
+                            mapid: 'mapbox.streets'
                         },
                         layerParams: {
                             showOnSelector: false
